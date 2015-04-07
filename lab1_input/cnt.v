3b77b50f4f8e6cf8299c2cf94386f18c0e7ef195
Apocalyptica <katerinaf1701@gmail.com>
2015-04-07 23:40:49 +0400

    Update cnt.v
    От счетчика идет сигнал на запись в fifo. Не нравится здоровое условие в if, но пока оставлю так. 
    P.S. Дорабатываю объединение модулей с fifo.

--- lab1_input/cnt.v
+++ lab1_input/cnt.v
@@ -1,7 +1,8 @@
-module cnt(clk,ena,res,upr,q);
+module cnt(clk,ena,res,upr,q,w_fifo);
 	input clk, res, ena;
 	output  reg[4:0] upr;
 	output  reg [5:0] q;
+	output  reg w_fifo;
 	
 	reg [5:0] cnt;
 	
@@ -11,10 +12,17 @@ module cnt(clk,ena,res,upr,q);
 		cnt<='b0;
 	else
 		if(ena)
+		begin
 			cnt<= cnt + 'b000001;
+			if ( (cnt == 'd0) || (cnt == 'd4) || (cnt == 'd8) || (cnt == 'd12) || (cnt == 'd16) || (cnt == 'd20) || (cnt == 'd24) || (cnt == 'd28))
+				w_fifo <= '1;
+			else
+				w_fifo <= '0;
+		end
 	q <= cnt;
 	case(cnt)
-	6'b000000: upr = 'b10000;
+	
+	6'b000000: upr = 'b10000; 
 	6'b000100: upr = 'b10001;
 	6'b001000: upr = 'b10010;
 	6'b001100: upr = 'b10011;
@@ -24,5 +32,7 @@ module cnt(clk,ena,res,upr,q);
 	6'b011100: upr = 'b10111;
 	default: upr = 'b0;
 	endcase
+	
+	
 	end
 endmodule
