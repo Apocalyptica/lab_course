4600e9d33d3aca4de4ccae61a623f15d06da0b5e
Apocalyptica <katerinaf1701@gmail.com>
2015-04-07 23:37:01 +0400

    Исправление недочёта.
    Исправлен модуль по рекомендации) Всё работает.

--- lab1_input/shift_reg.v
+++ lab1_input/shift_reg.v
@@ -1,11 +1,11 @@
 module shift_reg(clk,ena,data_i,data_o,ind);
 	input clk,ena;
 	input  [3:0] data_i;
-	output reg [15:0] data_o;
-	output reg [15:0] ind;
+	output  [15:0] data_o;
+	output  [15:0] ind;
 	
-	
-	reg [3:0] shift [0:3];
+	logic [3:0] shift [0:3];
+	logic [15:0] data_out;
 	integer i,j;
 	
 	always @(posedge clk)
@@ -21,15 +21,14 @@ module shift_reg(clk,ena,data_i,data_o,ind);
 	
 		for(i=0; i <=3; i=i+1)
 			for(j=0; j <= 3 ; j=j+1)	
-			begin
-				if(ena)
-					data_o[4*i+j] <= shift[j][i];
-				else
-					data_o[4*i+j] <= 'b0;
-			end
-			ind[3:0] <= shift[0];
-			ind[7:4] <= shift[1];
-			ind[11:8] <= shift[2];
-			ind[15:12] <= shift[3];		
+				data_out[4 * i+j] <= ena? shift[j][i]:'b0;
 	end	
-endmodule	
+	
+	assign data_o = data_out;
+	assign ind[3:0] = shift[0];
+	assign ind[7:4] = shift[1];
+	assign ind[11:8] = shift[2];
+	assign ind[15:12] = shift[3];
+	
+endmodule		
+	
